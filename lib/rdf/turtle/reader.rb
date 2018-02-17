540f3f081ed4826187f663d329a9929ad6ae64ba
Daniel Patton <danielpatton88@yahoo.com>
2018-02-16 22:50:42 -0700

    Update reader.rb

--- lib/rdf/turtle/reader.rb
+++ lib/rdf/turtle/reader.rb
@@ -167,7 +167,7 @@ module RDF::Turtle
     # @return [RDF::Statement] Added statement
     # @raise [RDF::ReaderError] Checks parameter types and raises if they are incorrect if parsing mode is _validate_.
     def add_statement(production, statement)
-      puts "add statement #{@lexer}"
+      puts "add statement #{statement}"
       error("Statement is invalid: #{statement.inspect.inspect}", production: produciton) if validate? && statement.invalid?
       @callback.call(statement) if statement.subject &&
                                    statement.predicate &&
